7bc4e346fd788989633a768477b484d34b96b736
adnylortym <adnylortym@gmail.com>
2020-08-18 10:34:09 +0200

    Divide the projects on src and tests

--- tests/SnakeGame.View.Tests/RendererTests.cs
+++ tests/SnakeGame.View.Tests/RendererTests.cs
@@ -1,6 +1,5 @@
 using Moq;
 using NUnit.Framework;
-using NUnit.Framework.Internal;
 using SnakeGame.Controller.ExternalInterfaces;
 using SnakeGame.Controller.Resources;
 using SnakeGame.View.ExternalInterfaces;
